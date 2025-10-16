ad60237a1e920b6375c904f741ab4a8f07ee48e1
Shin <penspulse@gmail.com>
2025-10-16 11:04:47 +0800

    chore: add deploy actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import remarkDirective from "remark-directive";
+import { defineConfig } from 'astro/config';
+import remarkDirective from 'remark-directive';
 
-import { remarkAdmonitions } from "./src/plugins/remark-admonitions.mjs";
+import { remarkAdmonitions } from './src/plugins/remark-admonitions.mjs';
 
-// https://astro.build/config
 export default defineConfig({
+  base: '/hello-astro',
+  site: 'https://penspulse326.github.io',
+
   markdown: {
     remarkPlugins: [remarkDirective, remarkAdmonitions],
   },
